32fd138bed4cb250b0227ab19104ed36201ddb90
Ksandro <1ksandroxhallo@gmail.com>
2020-12-06 10:47:04 +0100

    First Commit

---
+++ findSqrt.py
@@ -0,0 +1,20 @@
+from random import random,randrange
+import math
+
+
+def findSqrt(nr):
+    g = round(nr / randrange(1, nr))
+    while nr != g * g:
+        print(g * g, (g + nr / g) / 2)
+        if (nr == math.ceil(g * g)):
+            return g
+        else:
+            g = (g + nr / g) / 2
+
+
+findSqrt(6)
+
+
+
+
+
